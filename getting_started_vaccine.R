2c261c58942a3662a255fe9aaf308ae853b92ad0
Keo Morakod Ung <keomorakodung@gmail.com>
2024-10-30 12:08:43 +0000

    Updated getting_started_vaccine.R
    I initially created a copy and didn't specify it onto the same local path as my github repository (vaccine_data) so it was only on my computer in the local working directory (~Desktop/wd_vaccine) which is the setwd.

---
+++ getting_started_vaccine.R
@@ -0,0 +1,32 @@
+##Assignment #1 for module SOCS0100 
+
+#Setting up my directory 
+setwd("~/Desktop/wd_vaccine")
+
+#Setting up my packages
+
+if (!require("pacman")) {
+  install.packages("pacman")
+}
+library(pacman)
+pacman::p_load(
+  tidyverse, #including dplyr for processing and purrr for functions
+  kableExtra, #creating tables
+  flextable, #creating tables
+  glue, #to combine strings and objects
+  ggplot2) #dataviz
+
+options(scipen = 999) #managing scientific notation in data
+
+#Setting up for reproducibility
+
+if (!require("groundhog")) { 
+  install.packages("groundhog")
+  library(groundhog)
+}
+
+pkgs <- c("tidyverse", "kableExtra", "flextable", "glue", "ggplot2")
+groundhog.library(pkgs, "2024-10-30") #Freezing date to when I started the project
+
+#Data Processing and Functional Programming (25 points)
+
